93ee24a5d7a3f08abbafc8dd7c1dabbe76261e53
Abdulelah21 <78698991+Abdulelah21@users.noreply.github.com>
2021-04-07 22:12:21 +0300

    change about table view

--- src/java/BrowseAllRepairRecords.java
+++ src/java/BrowseAllRepairRecords.java
@@ -6,6 +6,10 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import static java.lang.System.out;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,69 +21,132 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class BrowseAllRepairRecords extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         response.setContentType("text/html;charset=UTF-8");
+
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet BrowseAllRepairRecords</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet BrowseAllRepairRecords at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            out.println("<!DOCTYPE html>\n"
+                    + "<html dir=\"ltr\">\n"
+                    + "\n"
+                    + "    <head>\n"
+                    + "        <meta charset=\"utf-8\">  \n"
+                    + "        <title>About us</title>\n"
+                    + "        <meta name=\"description\" content=\"An interactive getting started guide for Brackets.\">\n"
+                    + "        <link rel=\"stylesheet\" href=\"css/style.css\">\n"
+                    + "    </head>\n"
+                    + "    <body  >\n"
+                    + "        <header>\n"
+                    + "            <div class=\"container\">\n"
+                    + "                <a href=\"index.html\" id=\"logo\">\n"
+                    + "                    <img src=\"images/logo.png\">\n"
+                    + "\n"
+                    + "                </a>\n"
+                    + "                <nav id=\"navbar\">\n"
+                    + "                    <ul>\n"
+                    + "                        <li >\n"
+                    + "                            <a href=\"index.html\">Home</a>\n"
+                    + "                        </li>\n"
+                    + "                        <li>\n"
+                    + "                            <a href=\"AddRequest.html\">Add New Repair Request</a>\n"
+                    + "\n"
+                    + "                        </li>\n"
+                    + "                        <li class=\"current\">\n"
+                    + "                            <a href=\"BrowseAllRepairRecords\">Browse All Repair Records</a>\n"
+                    + "\n"
+                    + "                        </li>\n"
+                    + "                        <li ></li>\n"
+                    + "                        <a href=\"feedback.html\">Feedback</a>\n"
+                    + "\n"
+                    + "                        </li>\n"
+                    + "\n"
+                    + "                    </ul>\n"
+                    + "\n"
+                    + "                </nav>\n"
+                    + "\n"
+                    + "\n"
+                    + "            </div>\n"
+                    + "        </header>\n"
+                    + "        <div id=\"record\">\n"
+                    + "            <div class=\"container\">\n"
+                    + "                <h1>\n"
+                    + "                    All Repair Records\n"
+                    + "                </h1>\n"
+                    + "<div class = table>");
+
+            // here we but the table code 
+            out.println("<div>");
+
+            dbConnntion db = new dbConnntion();
+            Connection conn = db.getConnection();
+
+            String sql = "select * from repairs";
+
+            PreparedStatement pStmt = conn.prepareStatement(sql);
+            ResultSet rs = pStmt.executeQuery();
+
+            if (!rs.isBeforeFirst()) {
+                out.println("<p> No registration records in database! </p>");
+            } else {
+                out.println("<table border='1'>");
+
+                out.println("<tr> <th> ID </th> <th> Computer Owner </th> <th> Type </th> <th> Model </th>"
+                        + " <th> Problems </th> <th> Repair Status </th> <th> Repair Cost </th> <th> Repair Finish  </th> "
+                        + "<th> Repair Delivery </th></tr>");
+
+                  while (rs.next()) {
+
+                    out.println("<tr>");
+
+                    out.println("<td>" + rs.getInt("id") + "</td>");
+                    out.println("<td>" + rs.getString("Computer_Owner") + "</td>");
+                    out.println("<td>" + rs.getString("Type") + "</td>");
+                    out.println("<td>" + rs.getString("Model") + "</td>");
+                    out.println("<td>" + rs.getString("Problems") + "</td>");
+                    out.println("<td>" + rs.getString("Repair_Status") + "</td>");
+                    out.println("<td>" + rs.getFloat("Repair_Cost") + "</td>");
+                    out.println("<td>" + rs.getTimestamp("Repair_Finish_DateTime") + "</td>");
+                    out.println("<td>" + rs.getTimestamp("Repair_Delivery_DateTime") + "</td>");
+                    out.println("</tr>");
+
+                }
+                out.println("</table>");
+            }
+
+            conn.close();
+
+            out.print("</div>"
+                    + "\n"
+                    + "\n"
+                    + "\n"
+                    + "\n"
+                    + "            </div>\n"
+                    + "\n"
+                    + "        </div>\n"
+                    + "\n"
+                    + "        <footer>\n"
+                    + "            <div class=\"container\">\n"
+                    + "\n"
+                    + "                <p>Coded by Abed and Abdulelah with love</p>\n"
+                    + "                <p>All rights reserved &copy;</p>\n"
+                    + "\n"
+                    + "            </div>\n"
+                    + "        </footer>\n"
+                    + "\n"
+                    + "\n"
+                    + "    </body>\n"
+                    + "</html>\n"
+                    + "\n"
+                    + "");
+        } catch (Exception ex) {
+            //throw new ServletException(ex);
+            out.println("<p class=\"error\"> There was an error exception meesage: " + ex + "</p>");
+
+        } finally {
+
+            out.close();
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
